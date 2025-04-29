13d2333df93ad75e20e01d939abc200f87330e1c
M.Abdelkhalik <mohamed.abdelkhalik@wothoq.co>
2025-04-29 20:02:26 +0300

    Initial Commit

---
+++ AuthCyle/AuthCyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthCyleApp.swift
+//  AuthCyle
+//
+//  Created by Muhmmed Ahmed Abdelkhalik Elabd on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct AuthCyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthCyle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthCyle
+//
+//  Created by Muhmmed Ahmed Abdelkhalik Elabd on 29/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
